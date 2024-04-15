5d45ae0c8e53e4786dc795212a9b631dc56630dd
Jessica Pichardo <jess.pichardo18@gmail.com>
2024-04-14 17:32:07 -0700

    final

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// index.js
+
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element and assign it to the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// Set the id of the <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the innerHTML of the <h1> element to include the text "YOUR-NAME is the champion"
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// Append the <h1> element to the <body> element
+document.body.appendChild(newHeader);
